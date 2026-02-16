07145648ff9b0fe9d3978a36f9dfa85ba11cbe99
aceboll23 <cebolladler@gmail.com>
2026-02-15 20:22:26 -0800

    Added test file for logging in

---
+++ tests/BoredGamers.Tests/Account/LoginTests.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BoredGamers.Data;
+using BoredGamers.Models;
+using BoredGamers.Services.Games;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using NUnit.Framework;
+using Microsoft.AspNetCore.Identity;
+
