017a5b9079e41994863e890586754b9991e26040
Mr Logan Montgomery <lmontgomery23@mail.wou.edu>
2026-03-13 12:24:39 -0700

    Refactor: move helper methods into Playgroup model per professor feedback

--- src/BoredGamers/Models/PlaygroupMember.cs
+++ src/BoredGamers/Models/PlaygroupMember.cs
@@ -1,6 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema; //[ForeignKey]
 
 namespace BoredGamers.Models
 {
