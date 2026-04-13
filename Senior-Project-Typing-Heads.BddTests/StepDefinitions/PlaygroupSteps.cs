9ac610c71a900e2ea065fb4f3755cb9c4b6d5c8f
Mr Logan Montgomery <lmontgomery23@mail.wou.edu>
2026-04-13 15:36:49 -0700

    Fix CreatePlaygroup BDD tests to use seeded user instead of hardcoded PersonThree

--- Senior-Project-Typing-Heads.BddTests/StepDefinitions/PlaygroupSteps.cs
+++ Senior-Project-Typing-Heads.BddTests/StepDefinitions/PlaygroupSteps.cs
@@ -1,7 +1,8 @@
 using NUnit.Framework;
-using OpenQA.Selenium; 
+using OpenQA.Selenium;
 using Reqnroll;
 using Senior_Project_Typing_Heads.BddTests.Support;
+using System.Net.Http.Json;
 
 namespace Senior_Project_Typing_Heads.BddTests.StepDefinitions;
 
@@ -9,25 +10,49 @@ namespace Senior_Project_Typing_Heads.BddTests.StepDefinitions;
 public class PlaygroupSteps
 {
     private readonly WebDriverContext _webDriverContext;
+    private readonly BddPlaygroupSeedDataContext _playgroupSeedDataContext;
     private string _createdPlaygroupName = string.Empty;
 
-    public PlaygroupSteps(WebDriverContext webDriverContext)
+    private class ResetPlaygroupDataResponse
+    {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+
+    public PlaygroupSteps(WebDriverContext webDriverContext, BddPlaygroupSeedDataContext playgroupSeedDataContext)
     {
         _webDriverContext = webDriverContext;
+        _playgroupSeedDataContext = playgroupSeedDataContext;
     }
 
     [Given("I am logged in as PersonThree")]
     public void GivenIAmLoggedInAsPersonThree()
     {
+        using var httpClient = new HttpClient();
+
+        var resetResponse = httpClient
+            .PostAsync($"{TestSettings.BaseUrl}/dev/bdd/reset-playgroup-data", null)
+            .GetAwaiter()
+            .GetResult();
+
+        resetResponse.EnsureSuccessStatusCode();
+
+        var seedData = resetResponse.Content
+            .ReadFromJsonAsync<ResetPlaygroupDataResponse>()
+            .GetAwaiter()
+            .GetResult();
+
+        ArgumentNullException.ThrowIfNull(seedData, "Failed to read BDD playgroup seed data response.");
+
+        _playgroupSeedDataContext.Username = seedData.Username;
+        _playgroupSeedDataContext.Password = seedData.Password;
+
         var driver = _webDriverContext.Driver!;
-
         driver.Navigate().GoToUrl(TestSettings.BaseUrl + "/Account/Login");
-
-        driver.FindElement(By.Id("UsernameOrEmail")).SendKeys("PersonThree");
-        driver.FindElement(By.Id("Password")).SendKeys("123ABc!!!");
+        driver.FindElement(By.Id("UsernameOrEmail")).SendKeys(seedData.Username);
+        driver.FindElement(By.Id("Password")).SendKeys(seedData.Password);
         driver.FindElement(By.CssSelector("button[type='submit']")).Click();
 
-        // Wait for login to complete and redirect
         System.Threading.Thread.Sleep(1000);
     }
 
@@ -44,7 +69,7 @@ public class PlaygroupSteps
         Assert.That(form, Is.Not.Null);
     }
 
-        [When("I fill in the Create Playgroup form with a unique name")]
+    [When("I fill in the Create Playgroup form with a unique name")]
     public void WhenIFillInTheCreatePlaygroupFormWithAUniqueName()
     {
         _createdPlaygroupName = "Test Group BDD " + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss");
